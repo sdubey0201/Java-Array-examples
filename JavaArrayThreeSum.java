0a4457977fd738330214c5a21522cff558e3e1e6
sunil dubey <sdubey0201@gmail.com>
2024-12-14 22:55:49 +0530

    2 pointer approach

--- JavaArrayThreeSum.java
+++ JavaArrayThreeSum.java
@@ -6,9 +6,19 @@ import java.util.List;
 
 public class JavaArrayThreeSum {
     public static void main(String[] args) {
+//        To solve the "three-sum" problem,
+//        where you need to find unique triplets in the array
+//        that add up to zero, you can follow these steps:
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int targe = 0;
+        System.out.println(getThreeSum1(nums, targe).toString());
+//        nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+//        The distinct combinations are [-1,0,1] and [-1,-1,2].
+
 
     }
-    public static List<List<Integer>>getThreeSum(int [] values,int target){
+
+    public static List<List<Integer>> getThreeSum(int[] values, int target) {
 //        Output: [[-1,-1,2],[-1,0,1]]
 //        int [] nums = {-1,0,1,2,-1,-4};
 //        nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
@@ -24,16 +34,20 @@ public class JavaArrayThreeSum {
 //        The distinct combinations are [-1,0,1] and [-1,-1,2].
 //
 //        Notice that the order of the output does not matter
+        Arrays.sort(values);
+        System.out.println(Arrays.toString(values));
         List<List<Integer>> outPut = new ArrayList<>();
-        for (int i = 0;i<values.length;i++){
+        for (int i = 0; i < values.length; i++) {
             int a = values[i];
-            for (int j = i+1;j<values.length;j++){
+            for (int j = i + 1; j < values.length; j++) {
                 int b = values[j];
-                for (int k = j+1;k<values.length;k++){
+                for (int k = j + 1; k < values.length; k++) {
                     int c = values[k];
-                    int tempSum = a+b+c;
-                    if (target==tempSum){
-                        outPut.add(Arrays.asList(a,b,c));
+                    int tempSum = a + b + c;
+                    if (target == tempSum) {
+                        List<Integer> list = Arrays.asList(a, b, c);
+                        if (!outPut.contains(list))
+                            outPut.add(list);
                     }
                 }
 
@@ -44,7 +58,7 @@ public class JavaArrayThreeSum {
         return outPut;
     }
 
-    public static List<List<Integer>>getThreeSum1(int [] values,int target){
+    public static List<List<Integer>> getThreeSum1(int[] values, int target) {
 //        Output: [[-1,-1,2],[-1,0,1]]
 //        int [] nums = {-1,0,1,2,-1,-4};
 //        nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
@@ -61,10 +75,38 @@ public class JavaArrayThreeSum {
 //
 //        Notice that the order of the output does not matter
         List<List<Integer>> outPut = new ArrayList<>();
+        Arrays.sort(values);
+//        [-4, -1, -1, 0, 1, 2]
+        int length = values.length;
+        for (int i = 0; i < length; i++) {
+            int temp = values[i];
+            int left = i + 1;
+            int right = length - 1;
+            while (left < right) {
+                int lv = values[left];
+                int rv = values[right];
+                int tempSump = lv + rv + temp;
+                if (tempSump == target) {
+                    List<Integer> tempList = Arrays.asList(lv, rv, temp);
+                    if (!outPut.contains(tempList))
+                        outPut.add(tempList);
+
+                    left++;
+                    right--;
+
+                } else if (target>tempSump) {
+                 left++;
+                }else {
+                    right--;
+                }
+
+            }
+
+        }
 
 
         return outPut;
+
     }
-
-
 }
+
